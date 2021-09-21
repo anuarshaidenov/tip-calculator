f6e4065caf174c6247ee4d29074b6d2b22312003
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-09-21 21:34:40 +0600

    done active states for all elements and finalize the app

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Necessary DOM elements
 const inputBill = document.querySelector("#bill");
 const inputNumberOfPeople = document.querySelector("#people");
 const tipsContainer = document.querySelector(".tips");
@@ -7,15 +8,18 @@ const inputCustomActive = document.querySelector(".btn--custom");
 const amountPerPerson = document.querySelector("#amount-person");
 const amountTotal = document.querySelector("#amount-total");
 
-let totalTip = 0,
-  tipPerPerson = 0;
+/* FORMULAS */
 // total tip: bill * percentage / 100
 // tip per peson: total tip / number of people
 
+// Initial values
+let totalTip = 0,
+  tipPerPerson = 0;
 let bill = 0,
   tip = 0,
   numberOfPeople = 0;
 
+// Calculate and render total tip amount
 const calculateTotalTip = function () {
   totalTip = (Math.round(((bill * tip) / 100) * 100) / 100).toFixed(2);
   amountTotal.textContent = `$${totalTip}`;
@@ -23,6 +27,7 @@ const calculateTotalTip = function () {
   calculateTipPerPerson();
 };
 
+// Calculate tip per person
 const calculateTipPerPerson = function () {
   if (numberOfPeople <= 0) return;
   tipPerPerson = (Math.round((totalTip / numberOfPeople) * 100) / 100).toFixed(
@@ -31,6 +36,7 @@ const calculateTipPerPerson = function () {
   amountPerPerson.textContent = `$${tipPerPerson}`;
 };
 
+// Check if input value is valid or not
 const isValueZero = function (value) {
   if (value <= 0) {
     this.classList.add("input--error");
@@ -41,6 +47,7 @@ const isValueZero = function (value) {
   }
 };
 
+// Display error message when value is invalid for all three inputs
 [inputBill, inputCustomActive, inputNumberOfPeople].forEach((input) => {
   input.addEventListener("focus", function (e) {
     const value = e.target.value;
@@ -52,28 +59,20 @@ const isValueZero = function (value) {
   });
 });
 
-inputBill.addEventListener("input", function (e) {
-  const value = e.target.value;
-  if (!isValueZero.call(this, value)) {
-    bill = value;
-    calculateTotalTip();
-  }
-});
-inputCustomActive.addEventListener("input", function (e) {
-  const value = e.target.value;
-  if (!isValueZero.call(this, value)) {
-    tip = value;
-    calculateTotalTip();
-  }
-});
-inputNumberOfPeople.addEventListener("input", function (e) {
-  const value = e.target.value;
-  if (!isValueZero.call(this, value)) {
-    numberOfPeople = value;
-    calculateTotalTip();
-  }
+// Calculate and display the tip values whenever inputs change
+[inputBill, inputCustomActive, inputNumberOfPeople].forEach((input) => {
+  input.addEventListener("input", function (e) {
+    const value = e.target.value;
+    if (!isValueZero.call(this, value)) {
+      if (this.id === "bill") bill = value;
+      if (this.id === "tip") tip = value;
+      if (this.id === "people") numberOfPeople = value;
+      calculateTotalTip();
+    }
+  });
 });
 
+// Calculate and display total tip whenever the tip button gets clicked
 tipsContainer.addEventListener("click", function (e) {
   const btn = e.target.closest(".btn--tip");
   if (!btn) return;
@@ -85,6 +84,7 @@ tipsContainer.addEventListener("click", function (e) {
   calculateTotalTip();
 });
 
+// Reset all the values and render them
 btnReset.addEventListener("click", function () {
   if (this.classList.contains("btn--reset__active")) {
     // set bill = 0, tip = 0, numberofpeople = 0 then calculate and display the total tip
